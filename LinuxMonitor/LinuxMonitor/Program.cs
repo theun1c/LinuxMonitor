b14bff822ade9ecdc3589bd1bab6e8d1e638e97f
theun1c <belovdima1122@yandex.ru>
2025-06-09 04:30:30 +0300

    feat: rethought the project. now I use the process

--- LinuxMonitor/LinuxMonitor/Program.cs
+++ LinuxMonitor/LinuxMonitor/Program.cs
@@ -1,38 +1,20 @@
-﻿using System.Diagnostics;
+﻿using LinuxMonitor.BashExecutor;
 
 namespace LinuxMonitor
 {
     internal class Program
     {
         /// <summary>
-        /// NOTE: тест работы потоков
+        /// точка входа да
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            string cpuInfo = ExecuteLinuxCommand("cat /proc/stat | head -n 1");
-            Console.WriteLine(cpuInfo); // cpu  1234 567 890 123456 0 0 0 0 0 0
-        }
-
-        public static string ExecuteLinuxCommand(string command)
-        {
-            var process = new Process()
-            {
-                StartInfo = new ProcessStartInfo
-                {
-                    FileName = "/bin/bash",
-                    Arguments = $"-c \"{command}\"",
-                    RedirectStandardOutput = true,
-                    UseShellExecute = false,
-                    CreateNoWindow = true,
-                }
-            };
-
-            process.Start();
-            string output = process.StandardOutput.ReadToEnd();
-            process.WaitForExit();
-
-            return output;
+            LinuxExecutor linuxExecutor = new LinuxExecutor(); // потом мб перепишу 
+            linuxExecutor.Command = "free -h"; // задаем команлу 
+            var output = linuxExecutor.ExecuteLinuxCommand(linuxExecutor.Command); // передаем
+            Console.WriteLine(output); // выводим
         }
     }
 }
+            
